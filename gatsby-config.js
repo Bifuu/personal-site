99895cd98e246951793ccb14dabd878759c21245
Dustin Stover <DustinStover@gmail.com>
2020-02-18 15:46:13 -0500

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Dustin's tech thoughts`,
+    author: `Dustin Stover`,
+    description: `The personal blog of my thoughts and opinions`,
+    siteUrl: ``,
     social: {
-      twitter: `kylemathews`,
+      twitter: `BifuuPlays`,
     },
   },
   plugins: [
